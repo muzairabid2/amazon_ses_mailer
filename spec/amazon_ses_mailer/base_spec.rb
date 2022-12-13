2ca8427f8ee376b0b86a59e51f3ef6a78e14d272
Uzair Abid <muhammad.uzair@purelogics.new>
2022-12-13 13:40:22 +0500

    Add test cases for base class.

--- spec/amazon_ses_mailer/base_spec.rb
+++ spec/amazon_ses_mailer/base_spec.rb
@@ -24,6 +24,46 @@ RSpec.describe AmazonSesMailer::Base do
       expect(AmazonSesMailer::Base.initialize('val')).to eq([])
     end
 
+    it 'should return values into string' do
+      expect(AmazonSesMailer::Base.transform_value('string')).to eq('string')
+    end
+
+    it 'should not return string into array' do
+      expect(AmazonSesMailer::Base.transform_value('string')).not_to eq(["1","2","3"])
+    end
+
+    it 'should return empty string it nil encounters' do
+      expect(AmazonSesMailer::Base.transform_value(nil)).to eq('')
+    end
+
+    it 'should not return nil' do
+      expect(AmazonSesMailer::Base.transform_value(nil)).not_to eq(nil)
+    end
+
+    it 'should return array if it encounters a array' do
+      expect(AmazonSesMailer::Base.transform_value([1,2,3])).to eq(["1","2","3"])
+    end
+
+    it 'should not return array into string' do
+      expect(AmazonSesMailer::Base.transform_value([1,2,3])).not_to eq('string')
+    end
+
+    it 'should return hash if it encounters a hash' do
+      expect(AmazonSesMailer::Base.transform_value({value: "val"})).to eq({value: "val"})
+    end
+
+    it 'should not return hash into string' do
+      expect(AmazonSesMailer::Base.transform_value({value: "val"})).not_to eq('string')
+    end
+
+    it 'should return hash into json' do
+      expect(AmazonSesMailer::Base.process_merge_vars({value: "val"})).to eq({value: "val"}.to_json)
+    end
+
+    it 'should not return hash into hash' do
+      expect(AmazonSesMailer::Base.process_merge_vars({value: "val"})).not_to eq({value: "val"})
+    end
+
   end
 
 end
